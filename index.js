ebb4520f75d55cbeaa87f9d7751579d2c789671b
Umoh Udonwa <udonwajnr10@gmail.com>
2025-03-07 22:56:32 -0800

    commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const connectDb = require("./config/db.js");
+const ServerlessHttp = require("serverless-http");
 
 // CORS configuration
 const corsOptions = {
@@ -30,13 +31,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const sendResponse = (res, statusCode, message, data = null) => {
-return res.status(statusCode).json({
-    success: statusCode >= 200 && statusCode < 300,
-    message,
-    data,
+  return res.status(statusCode).json({
+      success: statusCode >= 200 && statusCode < 300,
+      message,
+      data,
 });
 };
-
+app.get("/.netlify/functions/index",(req,res)=>{
+  return res.json({message:"hello world"})
+})
 
 app.get("/", (req, res) => {
   sendResponse(res, 200, "Hello World! This is a success message");
@@ -46,6 +49,12 @@ app.use("/api/user",require("./route/userRoute.js"))
 app.use("/api/products",require("./route/productRoute.js"))
 app.use("/api/order",require("./route/orderRoute.js"))
 
+const handler = ServerlessHttp(app)
+
+module.exports.handler = async(event,context)=>{
+  const result = await handler(event,context);
+  return result
+}
 connectDb();
 app.listen(port, () => {
   console.log(`This is running on port ${port}`.green.bold);
